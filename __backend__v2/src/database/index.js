9503ad235f6a65224480eb7c56302ea36f7072a7
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-05-28 10:44:52 -0300

    Setup inicial

---
+++ __backend__v2/src/database/index.js
@@ -0,0 +1,24 @@
+// Loader dos Models
+
+import Sequelize from 'sequelize';
+import databaseConfig from '../config/database';
+
+const models = [];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    models
+      .map((model) => model.init(this.connection))
+      .map(
+        (model) => model.associate && model.associate(this.connection.models)
+      );
+  }
+}
+
+export default new Database(); // Cria uma nova instância de Database()
